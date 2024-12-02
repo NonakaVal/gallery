7e410ed9727706ab5100f71cef255975e6ee8c58
NonakaVal <nonaka96@outlook.com>
2024-12-02 10:29:50 -0300

    f

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import pandas as pd
 st.set_page_config(
     page_title="teste de galeria",
     page_icon=":material/table:",
+    layout="wide"
 
 
 
@@ -27,44 +28,7 @@ if url:
 
     tudo = gs_manager.read_sheet(url, "TODAS")
 
-
-data = pd.concat([controles, consoles], ignore_index=True)
-# Exibição da galeria
-cols_per_row = 7
-
-
-
-
-tab1, tab2, tab3, tab5 = st.tabs(["CONSOLES", "CONTROLES", "EDICOES", "TABELAS"])
-
-with tab1:
-    # Galeria para "CONSOLES"
-    rows = [consoles.iloc[i:i + cols_per_row] for i in range(0, len(consoles), cols_per_row)]
-    for row in rows:
-        cols = st.columns(cols_per_row)
-        for col, (_, item) in zip(cols, row.iterrows()):
-            with col:
-                st.image(item["IMG"], caption=item["TITLE"], width=80)
-
-with tab2:
-    # Galeria para "CONTROLES"
-    rows = [controles.iloc[i:i + cols_per_row] for i in range(0, len(controles), cols_per_row)]
-    for row in rows:
-        cols = st.columns(cols_per_row)
-        for col, (_, item) in zip(cols, row.iterrows()):
-            with col:
-                st.image(item["IMG"], caption=item["TITLE"], width=80)
-
-with tab3:
-    # Galeria para "EDICOES"
-    rows = [products.iloc[i:i + cols_per_row] for i in range(0, len(products), cols_per_row)]
-    for row in rows:
-        cols = st.columns(cols_per_row)
-        for col, (_, item) in zip(cols, row.iterrows()):
-            with col:
-                st.image(item["IMG"], caption=item["TITLE"], width=70)
-
-with tab5:
+    st.write("todos itens")
     st.dataframe(
         tudo,
         column_config={
@@ -72,6 +36,44 @@ with tab5:
                 "Preview", help="Preview da imagem", width='small'
             )
         }, use_container_width=True)
+
+    
+
+# data = pd.concat([controles, consoles], ignore_index=True)
+# # Exibição da galeria
+# cols_per_row = 7
+
+
+
+
+# tab1, tab2, tab3, tab5 = st.tabs(["CONSOLES", "CONTROLES", "EDICOES", "TABELAS"])
+
+# with tab1:
+#     # Galeria para "CONSOLES"
+#     rows = [consoles.iloc[i:i + cols_per_row] for i in range(0, len(consoles), cols_per_row)]
+#     for row in rows:
+#         cols = st.columns(cols_per_row)
+#         for col, (_, item) in zip(cols, row.iterrows()):
+#             with col:
+#                 st.image(item["IMG"], caption=item["TITLE"], width=80)
+
+# with tab2:
+#     # Galeria para "CONTROLES"
+#     rows = [controles.iloc[i:i + cols_per_row] for i in range(0, len(controles), cols_per_row)]
+#     for row in rows:
+#         cols = st.columns(cols_per_row)
+#         for col, (_, item) in zip(cols, row.iterrows()):
+#             with col:
+#                 st.image(item["IMG"], caption=item["TITLE"], width=80)
+
+# with tab3:
+#     # Galeria para "EDICOES"
+#     rows = [products.iloc[i:i + cols_per_row] for i in range(0, len(products), cols_per_row)]
+#     for row in rows:
+#         cols = st.columns(cols_per_row)
+#         for col, (_, item) in zip(cols, row.iterrows()):
+#             with col:
+#                 st.image(item["IMG"], caption=item["TITLE"], width=70)
 
 # with tab5:
 #     st.dataframe(
@@ -81,3 +83,12 @@ with tab5:
 #                 "Preview", help="Preview da imagem", width='small'
 #             )
 #         }, use_container_width=True)
+
+# # with tab5:
+# #     st.dataframe(
+# #         tudo,
+# #         column_config={
+# #             "IMG": st.column_config.ImageColumn(
+# #                 "Preview", help="Preview da imagem", width='small'
+# #             )
+# #         }, use_container_width=True)
